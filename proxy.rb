c8c9ba62e7e339a7c28d85e6c9ac087e63b4bbd0
Mihael Mutić <35368241+PH4NTOMiki@users.noreply.github.com>
2024-11-05 14:06:53 +0100

    Update proxy.rb

--- proxy.rb
+++ proxy.rb
@@ -4,115 +4,103 @@ require 'rest-client'
 require 'open-uri'
 require 'viddl-rb'
 require 'video_info'
+require 'fileutils'
 
-class Rack::Handler::WEBrick
-    class << self
-        alias_method :run_original, :run
-    end
-    def self.run(app, options={})
-        options[:DoNotReverseLookup] = true
-        run_original(app, options)
-    end
-end
-
-url = "http://youtube.com"
-last_link = "/youtube"
-param = nil
-error = nil
 set :port, 4567
 set :public_folder, 'public'
+set :views, 'views'
 
+# Configurations (should be set as environment variables)
+YOUTUBE_URL = "http://youtube.com"
+DELETE_PASSWORD = ENV['DELETE_PASSWORD'] || "linux123"  # Replace with an env var in production
+DOWNLOAD_PATH = ENV['DOWNLOAD_PATH'] || File.join(settings.public_folder, 'videos')
+
+# Ensure download directory exists
+FileUtils.mkdir_p(DOWNLOAD_PATH)
 
 get '/' do
-  send_file 'public/home.html'
+  send_file File.join(settings.public_folder, 'home.html')
 end
-
 
 get '/youtube' do
-	content_type = ''
-	body = open(url) {|f|
-  		content_type = f.content_type    
-  		f.read
-	}
-end
-	
-get '/error' do
-	"#{error}"
-end
-
-get '/clear' do
-  correct_pswrd = "linux123"
-  pswrd = params[:auth]
-  delCount = 0
-  file_count = 0
-  oldest_file_date = 0.0
-
-  if pswrd == correct_pswrd then
-    Dir["public/*.mp4"].each do |f|
-      file_count += 1
-      if (Time.now - File.stat(f).mtime).to_i / 86400.0 > oldest_file_date.to_f then
-          oldest_file_date = (Time.now - File.stat(f).mtime).to_i / 86400.0
-        end
-
-      if (Time.now - File.stat(f).mtime).to_i / 86400.0 >= 1.0 then
-        File.delete(f)
-        delCount+=1
-      end
-    end
-    "#{delCount} File(s) deleted out of #{file_count} total files. Oldest file is now #{oldest_file_date} days old!"
-  else
-    "Authentication failed."
+  begin
+    content = URI.open(YOUTUBE_URL) { |f| f.read }
+    content_type 'text/html'
+    content
+  rescue => e
+    status 500
+    "Error fetching content: #{e.message}"
   end
 end
 
-get '/watch' do
-    v = params[:v]
-    watchurl = url + "/watch?v=" + v
-    video_stuff = VideoInfo.new("#{watchurl}")
-
-    filename = ViddlRb.get_names("#{watchurl}")
-    filename = filename[0]
-    filename = File.basename(filename,File.extname(filename))
-    newname = filename
-    newname = newname.tr('[','(')
-    newname = newname.tr(']',')')
-    newname = newname.tr("'","")
-    newname = newname.tr('_','')
-    newname = newname.tr('#','')
-
-    if File.exist?("public/" +newname+".mp4") == false then
-      system("viddl-rb #{watchurl} --quality *:360:mp4 --downloader aria2c --save-dir C:/Users/jmckay/Desktop/RubyDev/public/")
-      File.rename("public/#{filename}.mp4", "public/#{newname}.mp4")
-    end
-
-
-    #<video width='1280' height='720' controls>
-    # <source src='#{newname}.mp4' type='video/mp4'>
-    #Your browser does not support the HTML5 video tag!
-    #</video>
-
-    "<html>
-    <body>
-    <h1>#{filename}</h1>
-    <form action='#{last_link}'>
-    <input type='submit' value='Go back to Youtube'/>
-    </form>
-    
-    <div>
-    <center>
-    <embed src='#{newname}.mp4' width='720' height='480' scale='tofit' autoplay='false'>
-
-    <object data='#{newname}.mp4' width='720' height='480'>
-    </object></center>
-    </div>
-
-    <h2><u>Description</u><h2>
-    <small>#{video_stuff.description}</small>
-    </body>
-    </html>"
+get '/error' do
+  "An error has occurred: #{params[:error]}"
 end
 
-not_found do  
-    param = request.path + "?" + request.query_string
-    RestClient.get(url + param)
+get '/clear' do
+  password = params[:auth]
+  return "Authentication failed." unless password == DELETE_PASSWORD
+
+  file_count = 0
+  del_count = 0
+  oldest_file_date = 0.0
+
+  Dir.glob("#{DOWNLOAD_PATH}/*.mp4") do |file|
+    file_count += 1
+    file_age_days = (Time.now - File.mtime(file)) / 86400.0
+    oldest_file_date = [oldest_file_date, file_age_days].max
+
+    if file_age_days >= 1.0
+      File.delete(file)
+      del_count += 1
+    end
+  end
+
+  "#{del_count} file(s) deleted out of #{file_count} total files. Oldest file is now #{oldest_file_date.round(2)} days old."
+end
+
+get '/watch' do
+  video_id = params[:v]
+  return redirect to('/error?error=Video+ID+missing') unless video_id
+
+  watch_url = "#{YOUTUBE_URL}/watch?v=#{video_id}"
+
+  begin
+    # Fetch video information
+    video_info = VideoInfo.new(watch_url)
+    sanitized_name = sanitize_filename(ViddlRb.get_names(watch_url)[0])
+
+    # Define file paths
+    temp_file = File.join(DOWNLOAD_PATH, "#{sanitized_name}_temp.mp4")
+    final_file = File.join(DOWNLOAD_PATH, "#{sanitized_name}.mp4")
+
+    # Download if not already present
+    unless File.exist?(final_file)
+      system("viddl-rb #{watch_url} --quality '*:360:mp4' --downloader aria2c --save-dir #{DOWNLOAD_PATH}")
+      File.rename(temp_file, final_file) if File.exist?(temp_file)
+    end
+
+    # Render HTML using ERB template
+    erb :watch, locals: { video_name: sanitized_name, video_description: video_info.description }
+
+  rescue => e
+    redirect to("/error?error=#{URI.encode(e.message)}")
+  end
+end
+
+not_found do
+  param = request.path + "?" + request.query_string
+  begin
+    RestClient.get(YOUTUBE_URL + param)
+  rescue RestClient::ExceptionWithResponse => e
+    status 404
+    "Not Found: #{e.message}"
+  end
+end
+
+helpers do
+  # Helper to sanitize filenames
+  def sanitize_filename(filename)
+    filename.gsub(/[^\w\s-]/, '').gsub(/_/, '').strip
+  end
 end
